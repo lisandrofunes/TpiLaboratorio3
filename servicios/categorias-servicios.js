20aaa83ca9b749fdd67c74ec95d14fb1a040e88c
Brusa91 <118452350+Brusa91@users.noreply.github.com>
2023-11-09 10:44:18 -0300

    Creación categorías-servicios

---
+++ servicios/categorias-servicios.js
@@ -0,0 +1,55 @@
+const url = "https://65498855e182221f8d51a5f0.mockapi.io/api/TpiLab3/categorias";
+
+//API-REST CATEGORIAS//
+
+async function listar(id) {
+    let cadUrl;
+    if(isNaN(id))
+      cadUrl= url;
+    else 
+      cadUrl = url + "/" + id;  
+    return await fetch(cadUrl)
+        .then(respuesta => respuesta.json());
+}
+
+async function crear(descripcion) {
+
+    return await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            descripcion: descripcion
+        })
+    })
+}
+
+async function editar(id, descripcion) {
+
+    let urlPut = url + "/" + id;
+    return await fetch(urlPut, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            descripcion: descripcion
+        })
+    })
+}
+
+async function borrar(id){
+  
+    let urlPut = url + "/" + id;
+    return await fetch(urlPut, {
+            method: 'DELETE'
+       })
+}
+
+export const categoriasServices = {
+    listar,
+    crear,
+    editar,
+    borrar
+}
