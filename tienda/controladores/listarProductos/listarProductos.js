e72c51bcb5fe2a84fd9cdf4c0c26f3b8d96273d8
Brusa91 <brusadinleonardo@gmail.com>
2023-11-23 19:13:19 -0300

    listarProductos terminado

--- tienda/controladores/listarProductos/listarProductos.js
+++ tienda/controladores/listarProductos/listarProductos.js
@@ -7,15 +7,15 @@ function htmlCategoria(id, categoria) {
     /*SE DEBERÁ CONCATENAR PARA INCORPORAR EL id DE LA CATEGORIA AL ATRIBUTO data-idCategoria  */
     /*Y ADEMAS REEMPLAZAR EL TEXTO Nombre de Categoría POR EL VALOR QUE LLEGA AL PARAMETRO CATEGORIA DE LA FUNCION*/
     /*POR ULTIMO, LA FUNCION DEVOLVERA LA CADENA RESULTANTE*/
-    const htmlCategoriaString = 
+    const stringHtmlCategoria =
     `<div class="categoria" data-idCategoria="${id}">
-        <h1 class="categoria">${categoria}</h1>
-        <div class="productos">
-            <!-- Aca se listan los productos-->
-            <p class="item-producto">Sin productos.</p>
-        </div>
-    </div>`;
-    return(htmlCategoriaString);
+         <h1 class="categoria">${categoria}</h1>
+         <div class="productos">
+             <!-- Aca se listan los productos-->
+             <p class="item-producto">Sin productos.</p>
+         </div>
+     </div>`;
+    return(stringHtmlCategoria);
 
 }
 
@@ -30,9 +30,16 @@ function htmlItemProducto(id, imagen, nombre, precio) {
      *   let cadena = `Hola, ${titulo} Claudia  en que podemos ayudarla`;
      *   
     */
+   const stringHtmlItemProducto = 
+   `<div class="item-producto">
+        <img src="${imagen}" >
+        <p class="producto_nombre" name="motorola">${nombre}</p>
+        <p class="producto_precio">${precio}</p>
 
+        <a href="?idProducto=${id}#vistaProducto" type="button" class="producto_enlace" >Ver producto</a>
 
-
+    </div>`;
+    return(stringHtmlItemProducto);
 }
 
 async function asignarProducto(id) {
@@ -41,9 +48,14 @@ async function asignarProducto(id) {
     /*3- EN EL INTERIOR DEL BUCLE DEBERA LLAMAR A LA FUNCION htmlItemProducto y acumular su resultado en una cadena de caracteres */
     /*4- LUEGO DEL BUCLE Y CON LA CADENA RESULTANTE SE DEBE CAPTURAR EL ELEMENTO DEL DOM PARA ASIGNAR ESTOS PRODUCTOS DENTRO DE LA CATEGORIA CORRESPONDIENTE */
     /*5- PARA ELLO PODEMOS HACER USO DE UN SELECTOR CSS QUE SELECCIONE EL ATRIBUTO data-idCategoria=X, Ó LA CLASE .productos  .SIENDO X EL VALOR LA CATEGORIA EN CUESTION.*/
-
-
-
+    var listaProductosCategoria = productosServices.listarPorCategoria(id);
+    var stringHtmlNuevo ;
+    for (let i = 0 ; i < length(listaProductosCategoria); i++){
+        var stringProducto = htmlItemProducto(listaProductosCategoria[i][0], listaProductosCategoria[i][3], listaProductosCategoria[i][1], listaProductosCategoria[i][6]);
+        stringHtmlNuevo + stringProducto;
+    }
+    var categoria = document.querySelector(`data-idCategoria="${id}"`);
+    categoria.innerHTML(stringHtmlNuevo);
 
 }
 export async function listarProductos() {
@@ -54,7 +66,13 @@ export async function listarProductos() {
     /* 4- SE DEBERA ASIGNAR EL RESULTADO DE FUNCION ANTERIOR AL ELEMENTO DEL DOM .seccionProductos */
     /* 5- LUEGO DEBERÁ LLAMAR UNA FUNCION, asignarProducto, QUE RECIBA COMO PARAMETRO EL ID DE LA CATEGORIA  */
     /* 6- FIN DEL BUCLE Y FIN DE LA FUNCION */
-
+    var sectionProductos = document.querySelector(".seccionProductos");
+    var listaCategorias = categoriasServices.listar('todas');
+    for (let i = 0; i<length(listaCategorias); i++){
+        var constructorCategoria = htmlCategoria(listaCategorias[i][0], listaCategorias[i][1]);
+        sectionProductos.innerHTML(constructorCategoria);
+        asignarProducto(listaCategorias[i][0])
+    }
 
 }
 
