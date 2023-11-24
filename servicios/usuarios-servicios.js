996ee65416bdd52ac39535db2f52e65000fed9eb
Emanuel Celis <ema_celis@hotmail.com>
2023-11-23 21:18:29 -0300

    modificacion en el readme, creacion de nuevos mockapi

--- servicios/usuarios-servicios.js
+++ servicios/usuarios-servicios.js
@@ -1,4 +1,4 @@
-const url = "https://65418746f0b8287df1fe755a.mockapi.io/api/TpiLab3/usuarios";
+const url = "https://655fe5ad83aba11d99cff046.mockapi.io/api/TpiLab3/users";
 
 //API-REST USUARIOS//
 
